c1fea24c1b3e776b85110da1a5e4788748079809
Greg Stinson <gstinsonjr17@gmail.com>
2020-10-17 17:11:58 -0700

    commiting changes 2

--- script.js
+++ script.js
@@ -3,15 +3,30 @@ var generateBtn = document.querySelector("#generate");
 
 //generate password function
 function generatePassword(){
-    var UpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    var LowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    var lowerCase = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     var length = prompt('How long would you like this password to be?')
     var numbers = ['0','1','2','3','4','5','6','7','8','9']
     var specialChars =['!','@','#','$','%','^','&','*','(',')','?','<','>','-']
     var nums = confirm('Do you want numbers in your password?')
     var lcConfirm = confirm('Do you want lowercase letters in your password?')
-    carucConfirm = confirm('Do you want uppercase letters in your password?')
+    var ucConfirm = confirm('Do you want uppercase letters in your password?')
+    var scConfirm = confirm('Do you want special characters in your password?')
+    var passArray = upperCase
 
+    if (lcConfirm){
+        passArray = passArray.concat(lowerCase)
+    }
+    if(nums){
+        passArray = passArray.concat(numbers)
+    }
+    if(scConfirm){
+        passArray = passArray.concat(specialChars)
+    }
+
+    for (var i =1; i <= length; i++){
+        console.log(passArray[Math.floor(Math.random() * passArray.length)])
+    }
 
 }
 
